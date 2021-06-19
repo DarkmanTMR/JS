3b6b2711efafc68630c46c537041a9b21c162681
DarkmanTMR <trp1prt1@gmail.com>
2021-06-20 02:46:21 +0300

    js code completed

---
+++ lesson 1/l1.js
@@ -0,0 +1,204 @@
+
+// - Створити змінні. Присвоїти кожній з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//   Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+// let hi ='Hello';
+// let text0 = 'OWU';
+// let text1 = 'com';
+// let text2 = 'ua';
+// let number0 = 1;
+// let number1 = 10; 
+// let number2 = -999; 
+// let number3 = 123; 
+// let number4 = 3.14; 
+// let number5 = 2.7; 
+// let number6 = 16; 
+// let right = true;
+// let left = false;
+
+// console.log(hi);
+// console.log(text0);
+// console.log(text1);
+// console.log(text2);
+// console.log(number0);
+// console.log(number1);
+// console.log(number2);
+// console.log(number3);
+// console.log(number4);
+// console.log(number5);
+// console.log(right);
+// console.log(left);
+
+
+// document.write(`<h1>${hi}<h1>`);
+// document.write(`<h2>${text0}</h2>`);
+// document.write(`<h2>${text1}</h2>`);
+// document.write(`<h2>${text2}</h2>`);
+// document.write(`<h2>${number0}</h2>`);
+// document.write(`<h2>${number1}</h2>`);
+// document.write(`<h2>${number2}</h2>`);
+// document.write(`<h2>${number3}</h2>`);
+// document.write(`<h2>${number4}</h2>`);
+// document.write(`<h2>${number5}</h2>`);
+// document.write(`<h2>${number6}</h2>`);
+// document.write(`<h2>${right}</h2>`);
+// document.write(`<h2>${left}</h2>`);
+
+
+
+// alert(hi);
+// alert(text0);
+// alert(text1);
+// alert(text2);
+// alert(number0);
+// alert(number1);
+// alert(number2);
+// alert(number3);
+// alert(number4);
+// alert(number5);
+// alert(number6);
+// alert(right);
+// alert(left);
+
+// Переприсвоїти кожну змінну з завдання вище на будь які довільні значення.
+//   Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+
+// let hi ='One';
+// let text0 = 'day';
+// let text1 = 'left';
+// let text2 = 'second';
+// let number0 = 'day';
+// let number1 = 'is'; 
+// let number2 = 'comming'; 
+// let number3 = 5; 
+// let number4 = 4; 
+// let number5 = 3; 
+// let number6 = 2; 
+// let right = 1;
+// let left = 'Yeeeeey!!';
+
+// console.log(hi);
+// console.log(text0);
+// console.log(text1);
+// console.log(text2);
+// console.log(number0);
+// console.log(number1);
+// console.log(number2);
+// console.log(number3);
+// console.log(number4);
+// console.log(number5);
+// console.log(right);
+// console.log(left);
+
+// document.write(`<h1>${hi}<h1>`);
+// document.write(`<h2>${text0}</h2>`);
+// document.write(`<h2>${text1}</h2>`);
+// document.write(`<h2>${text2}</h2>`);
+// document.write(`<h2>${number0}</h2>`);
+// document.write(`<h2>${number1}</h2>`);
+// document.write(`<h2>${number2}</h2>`);
+// document.write(`<h2>${number3}</h2>`);
+// document.write(`<h2>${number4}</h2>`);
+// document.write(`<h2>${number5}</h2>`);
+// document.write(`<h2>${number6}</h2>`);
+// document.write(`<h2>${right}</h2>`);
+// document.write(`<h2>${left}</h2>`);
+
+// alert(hi);
+// alert(text0);
+// alert(text1);
+// alert(text2);
+// alert(number0);
+// alert(number1);
+// alert(number2);
+// alert(number3);
+// alert(number4);
+// alert(number5);
+// alert(number6);
+// alert(right);
+// alert(left);
+
+// Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. Зконкатенувати їх в одну змінну person.
+
+
+// let firstname = 'Tim';
+//  let middleName = 'Uralovych';
+//  let lastName = 'Abdurazakov';
+//  let person = firstname + ' ' + middleName +' ' + lastName;
+//  console.log(person);
+
+
+// За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+//   Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+
+
+// let age = prompt('how old are you?');
+// let name = prompt('what is your name?');
+// let faname = prompt('what is yours father name?');
+
+// console.log("Вітаю "+name+" "+faname+" тобі "+age+" років")
+
+
+
+// За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//   let a = 100; let b = '100'; let c = true;
+
+// let a = 100;
+// let b = 100;
+// let c = true;
+
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+
+// Поставте відповідний оператор в виразах що б вийшов відповідний результат.
+// В однакових виразах не використовувати однакові оператори!!!
+//   5 ? 6 -> true
+//   5 ? 6 -> false
+//   5 ? 6 -> false
+//   5 ? 6 -> false
+//   10 ? 10 -> true
+//   10 ? 10 -> true
+//   10 ? 10 -> false
+//   10 ? 10 -> false
+//   10 ? 10 -> false
+//   123 ? '123' -> false
+//   123 ? '123' -> true
+
+//
+// console.log(5<6);
+// console.log(5>6);
+// console.log(5>=6);
+// console.log(5===6);
+// console.log(10===10);
+// console.log(10>=10);
+// console.log(10>10);
+// console.log(10<10);
+// console.log(10!=10);
+// console.log(123>123);
+// console.log(123===123);
+
+// Додатково:
+//     - Подивіться на наступні вирази і спробуйте вгадати яким буде вивід в консоль. Перевірте себе.
+// console.log(132 > 100 && 45 < 12 ); false
+// console.log(34 > 33 && 23 < 90 ); true
+// console.log(99 > 100 && 45 > 12 ); false
+// console.log(132 > 100 || 45 < 12 ); true
+// console.log(111 > 11 || 45 < 111 ); true
+// console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) ); true
+// console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) ); true
+// console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) ); false
+// console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 )); true
+// console.log(!!'-1'); true
+// console.log(!!-1); true
+// console.log(!!'0'); true
+// console.log(!!'null'); true
+// console.log(!!'undefined'); true
+// console.log(!!(3/'owu')); false
+// console.log((111 > 11 || 45 < 111) ||  !!'0'); true
+// console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 )); false
+
+
+
